aececc9f30638c5a26a76ae2e72805f07ce5f42a
Hefaj <hefajstore@gmail.com>
2020-03-21 18:17:21 +0100

    Files

---
+++ SudokuConsole/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace SudokuConsole
+{
+    class Program
+    {
+        static int[,] matrix = new int[,]
+        {
+            { 9,0,2,4,1,5,0,0,0},
+            { 0,0,5,0,6,0,0,0,0},
+            { 3,7,0,0,0,0,0,6,1},
+
+            { 2,1,0,3,9,6,0,0,5},
+            { 4,0,6,0,0,0,0,0,2},
+            { 0,0,3,0,8,0,1,9,0},
+
+            { 6,4,9,0,3,1,0,5,7},
+            { 5,0,0,6,0,0,0,0,4},
+            { 8,0,7,5,0,9,0,0,0}
+        };
+
+        static void Print()
+        {
+            Console.WriteLine("*************************");
+            for (int i = 0; i < 9; i++)
+            {
+                if (i != 0 && i % 3 == 0) Console.WriteLine("--------------------");
+                for (int j = 0; j < 9; j++)
+                {
+                    if (j != 0 && j % 3 == 0) Console.Write("|");
+                    Console.Write(matrix[i, j].ToString() + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static bool Check(int y, int x, int n)
+        {
+            for (int i = 0; i < 9; i++)
+                if (matrix[y,i] == n) return false;
+
+            for (int i = 0; i < 9; i++)
+                if (matrix[i, x] == n) return false;
+
+            int _x = (int)(x / 3) * 3;
+            int _y = (int)(y / 3) * 3;
+
+            for (int i = 0; i < 3; i++)
+                if (matrix[_y+i,_x+i] == n) return false;
+
+            return true;
+        }
+
+        static void FindAns()
+        {
+            for (int y = 0; y < 9; y++)
+            {
+                for (int x = 0; x < 9; x++)
+                {
+                    if (matrix[y, x] == 0)
+                    {
+                        for (int n = 1; n < 10; n++)
+                        {
+                            if (Check(y, x, n))
+                            {
+                                matrix[y, x] = n;
+                                FindAns();
+                                matrix[y, x] = 0;
+                            }
+                        }
+                        return;
+                    }
+                }
+            }
+            Print();
+            Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Print();
+            FindAns();
+        }
+    }
+}
